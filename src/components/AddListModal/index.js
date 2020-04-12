ecfe76502e4f8d74c76ce09c1a3cebe0538e6959
Daniel Oliveira <dan.oliveira461@gmail.com>
2020-04-12 18:44:06 -0300

    [ADD] Criando layout da tela de adicionar categoria

--- src/components/AddListModal/index.js
+++ src/components/AddListModal/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
-import {Container, ButtomDismiss} from './styles';
+import {
+  KeyboardAvoiding,
+  ButtomDismiss,
+  Content,
+  Title,
+  Input,
+  Submit,
+  Label,
+} from './styles';
 import {colors} from '~/styles/index';
 
 export default function AddListModal({closeModal}) {
   return (
-    <Container behavior="padding">
+    <KeyboardAvoiding behavior="padding">
       <ButtomDismiss onPress={closeModal()}>
         <AntDesign name="close" color={colors.black} size={24} />
       </ButtomDismiss>
-    </Container>
+
+      <Content>
+        <Title>Create Todo List</Title>
+        <Input placeholder={'List name?'} />
+        <Submit>
+          <Label>Create</Label>
+        </Submit>
+      </Content>
+    </KeyboardAvoiding>
   );
 }
--- src/components/AddListModal/styles.js
+++ src/components/AddListModal/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
+import {colors} from '~/styles/index';
 
-export const Container = styled.KeyboardAvoidingView`
+export const KeyboardAvoiding = styled.KeyboardAvoidingView`
   flex: 1;
   justify-content: center;
   align-items: center;
@@ -10,4 +11,42 @@ export const ButtomDismiss = styled.TouchableOpacity`
   position: absolute;
   top: 64px;
   right: 32px;
+`;
+
+export const Content = styled.View`
+  align-self: stretch;
+  margin: 0px 32px;
+`;
+
+export const Title = styled.Text`
+  font-size: 28px;
+  font-weight: 800;
+  color: ${colors.black};
+  align-self: center;
+  margin-bottom: 16px;
+`;
+
+export const Input = styled.TextInput`
+  border-width: 1px;
+  border-color: ${colors.blue};
+  border-radius: 6px;
+  height: 50px;
+  margin-top: 8px;
+  padding: 0px 16px;
+  font-size: 18px;
+`;
+
+export const Submit = styled.TouchableOpacity`
+  margin-top: 24px;
+  height: 50px;
+  border-radius: 6px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.blue};
+`;
+
+export const Label = styled.Text`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${colors.white};
 `;
