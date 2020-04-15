06bce18b094529bde9e83981ebc305ce9d2820e2
Daniel Oliveira <dan.oliveira461@gmail.com>
2020-04-14 23:56:47 -0300

    [ADD] Criando header mostrando o status das tasks e quantidade ja concluidas

--- src/components/TodoModal/index.js
+++ src/components/TodoModal/index.js
@@ -1,7 +1,15 @@
 import React, {useState} from 'react';
+import {FlatList} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {colors} from '~/styles/index';
-import {Container, ButtomDismiss, Header, Title, Subtitle} from './styles';
+import {
+  Container,
+  ButtomDismiss,
+  Header,
+  Title,
+  Subtitle,
+  Section,
+} from './styles';
 
 export default function TodoModal({list, closeModal}) {
   const [name, setName] = useState(list.name);
@@ -11,18 +19,30 @@ export default function TodoModal({list, closeModal}) {
   const taskCount = todos.length;
   const completed = todos.filter((todo) => todo.completed).length;
 
+  const renderTodos = (item) => {
+    return <Title>{item.title}</Title>;
+  };
+
   return (
     <Container>
       <ButtomDismiss onPress={closeModal}>
         <AntDesign name="close" color={colors.black} size={24} />
       </ButtomDismiss>
 
-      <Header>
+      <Header color={color}>
         <Title>{name}</Title>
         <Subtitle>
           {completed} of {taskCount} tasks
         </Subtitle>
       </Header>
+
+      <Section>
+        <FlatList
+          data={todos}
+          keyExtractor={(item) => item.title}
+          renderItem={({item}) => renderTodos(item)}
+        />
+      </Section>
     </Container>
   );
 }
--- src/components/TodoModal/styles.js
+++ src/components/TodoModal/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import {colors} from '~/styles/index';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
@@ -12,8 +13,26 @@ export const ButtomDismiss = styled.TouchableOpacity`
   right: 32px;
 `;
 
-export const Header = styled.View``;
+export const Header = styled.View`
+  flex: 1;
+  align-self: stretch;
+  justify-content: flex-end;
+  margin-left: 64px;
+  border-bottom-width: 3px;
+  border-bottom-color: ${(props) => props.color};
+`;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-size: 30px;
+  font-weight: 800;
+  color: ${colors.black};
+`;
 
-export const Subtitle = styled.Text``;
+export const Subtitle = styled.Text`
+  margin-top: 4px;
+  margin-bottom: 16px;
+  color: ${colors.gray};
+  font-weight: 600;
+`;
+
+export const Section = styled.View``;
