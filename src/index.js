7abdaa388a153d189dc486a10a32751484fa9f9b
Daniel Oliveira <dan.oliveira461@gmail.com>
2020-04-30 23:57:57 -0300

    [ADD] criando screen de singin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import '~/config/ReactotronConfig';
 import Routes from '~/routes';
+import { NavigationContainer } from '@react-navigation/native';
 
-const App = () => <Routes />;
+const App = () => (
+  <NavigationContainer>
+    <Routes />
+  </NavigationContainer>
+);
 
 export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,31 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from '~/pages/Home';
+import SignIn from '~/pages/SignIn';
 
-const Routes = createAppContainer(createSwitchNavigator({ Home }));
+const userAUthenticated = true;
+const isSignout = false;
+
+const Stack = createStackNavigator();
+
+const Routes = () => (
+  <Stack.Navigator>
+    {userAUthenticated ? (
+      // No token found, user isn't signed in
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{
+          title: 'Sign in',
+          animationTypeForReplace: isSignout ? 'pop' : 'push',
+        }}
+      />
+    ) : (
+      <Stack.Screen name="Home" component={Home} />
+    )}
+  </Stack.Navigator>
+);
 
 export default Routes;
