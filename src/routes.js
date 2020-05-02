6c170a289f18caadb1cc15db64d309f9b788f00d
Daniel Oliveira <dan.oliveira461@gmail.com>
2020-05-02 12:34:18 -0300

    [ADD] Gerenciando rotas de acordo com estado o usuário se esta logado ou não

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from '~/pages/Home';
 import SignIn from '~/pages/SignIn';
 
-const isSignout = false;
-
 const Stack = createStackNavigator();
 
-const Routes = () => (
-  <Stack.Navigator headerMode="none">
-    <Stack.Screen
-      name="SignIn"
-      component={SignIn}
-      options={{
-        title: 'Sign in',
-        animationTypeForReplace: isSignout ? 'pop' : 'push',
-      }}
-    />
-    <Stack.Screen name="Home" component={Home} />
-  </Stack.Navigator>
-);
+const Routes = () => {
+  const signed = useSelector((state) => state.auth.signed);
+  const signout = useSelector((state) => state.auth.signout);
+
+  return (
+    <Stack.Navigator headerMode="none">
+      {signed ? (
+        <Stack.Screen name="Home" component={Home} />
+      ) : (
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{
+            title: 'Sign in',
+            animationTypeForReplace: signout ? 'pop' : 'push',
+          }}
+        />
+      )}
+    </Stack.Navigator>
+  );
+};
 
 export default Routes;
