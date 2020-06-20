7e944bf9bd14a01aeadedacb630c9f9ecaddcb6a
Daniel Oliveira <dan.oliveira461@gmail.com>
2020-06-20 13:51:35 -0300

    [PATCH] Ajustes e melhorias na tela de cadastro, adicionando novos campos e um novo layout

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -8,40 +8,51 @@ export const Container = styled.KeyboardAvoidingView.attrs({
 })`
   flex: 1;
   justify-content: center;
-  padding: 0 15px;
-  background-color: ${colors.background};
+  background-color: ${colors.primary};
 `;
 
 export const Header = styled.View`
+  flex: 1.5;
   justify-content: center;
   align-items: center;
 `;
 
-export const Logo = styled.View`
-  width: 160px;
-  height: 160px;
+export const Image = styled.Image.attrs({
+  resizeMode: 'contain',
+})`
   align-self: center;
-  border-radius: 100px;
-  background-color: ${colors.primary};
-  justify-content: center;
-  align-items: center;
+  width: 180px;
+  height: 180px;
 `;
 
-export const LogoName = styled.Text`
-  color: ${colors.white};
-  font-weight: 800;
+export const Body = styled.View.attrs({
+  borderTopRightRadius: 30,
+  borderTopLeftRadius: 30,
+})`
+  flex: 3;
+  justify-content: flex-start;
+  background-color: ${colors.white};
+  padding: 0px 15px;
+  box-shadow: 5px 5px 20px ${colors.blackPrimary};
+`;
+
+export const Title = styled.Text`
   font-size: 20px;
-`;
-
-export const Body = styled.View`
-  align-self: stretch;
-  margin-top: 50px;
+  color: ${colors.secondary};
+  font-weight: bold;
+  margin: 40px 0 20px 5px;
 `;
 
 export const Link = styled.TouchableOpacity``;
 
 export const FieldsForm = styled.View`
   margin-bottom: 24px;
+  flex: 0.7;
+  justify-content: flex-start;
+`;
+
+export const Grid = styled.View`
+  flex-direction: row;
 `;
 
 export const Label = styled.Text`
