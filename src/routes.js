10cc94a4c3a5403c2f55c7ca236d198bad15467a
Daniel Mobills <daniel@mobills.com>
2021-01-23 20:35:05 -0300

    chore: separating routes into components and creating a custom header for the main routes

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,49 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { createStackNavigator } from '@react-navigation/stack';
+import { format } from 'date-fns';
 
+import Header from '~/components/Header';
+
+import Dashboard from '~/pages/Dashboard';
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
-import Dashboard from '~/pages/Dashboard';
 
-const Routes = () => {
-  const signed = useSelector((state) => state.auth.signed);
-  const signout = useSelector((state) => state.auth.signout);
-
+export default function Routes() {
   const Stack = createStackNavigator();
 
-  return (
-    <Stack.Navigator headerMode="none">
-      {signed ? (
-        <Stack.Screen name="Dashboard" component={Dashboard} />
-      ) : (
-        <>
-          <Stack.Screen
-            name="SignIn"
-            component={SignIn}
-            options={{
-              animationTypeForReplace: signout ? 'pop' : 'push',
-            }}
-          />
-          <Stack.Screen name="SignUp" component={SignUp} />
-        </>
-      )}
-    </Stack.Navigator>
-  );
-};
+  const { signed } = useSelector((state) => state.auth);
 
-export default Routes;
+  const LoginStack = () => {
+    return (
+      <Stack.Navigator headerMode="none">
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    );
+  };
+
+  const MainStack = () => {
+    return (
+      <Stack.Navigator
+        headerMode="screen"
+        screenOptions={{
+          header: ({ scene, previous, navigation }) => (
+            <Header scene={scene} previous={previous} navigation={navigation} />
+          ),
+        }}
+      >
+        <Stack.Screen
+          name="Dashboard"
+          component={Dashboard}
+          options={{
+            title: 'Hey, Daniel Oliveira',
+            subtitle: format(new Date(), 'PP'),
+          }}
+        />
+      </Stack.Navigator>
+    );
+  };
+
+  return signed ? <MainStack /> : <LoginStack />;
+}
