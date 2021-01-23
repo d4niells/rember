ef82a1790fa1ad1aa00b5ad63ffd6c86b4bbec3e
Daniel Mobills <daniel@mobills.com>
2021-01-23 20:15:09 -0300

    chore: update index.js removing provider theme for styled-components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components/native';
 
-import { store } from './store';
-import App from './App';
+import { store } from '~/store';
+import App from '~/App';
 
-import { light } from '~/styles/theme';
-
-function Index() {
+export default function Index() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={light}>
-        <App />
-      </ThemeProvider>
+      <App />
     </Provider>
   );
 }
-
-export default Index;
